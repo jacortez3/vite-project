b831cb1dbc195da3b8b86c1616474707bf140136
Jonathan Cortez <jacortez3@espe.edu.ec>
2024-01-19 18:04:55 -0500

    Deber3-final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import InputExample from './components/ExampleInput';
 function App() {
   return (
     <div>
-      <h1>Ejemplos de Eventos en React</h1>
       <ClickExample />
       <InputExample />
     </div>
